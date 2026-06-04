8fa2037d6b334dae93f68cae86aed29296fabdcf
lucasxie-dev <lucasxie.dev@gmail.com>
2026-06-04 11:03:54 +0800

    commit

---
+++ src/main/java/dev/lucasxie/learning/LearningPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.lucasxie.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lucasxie/learning/LearningPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lucasxie.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
